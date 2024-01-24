48bff8428128387cba043ad589c17f9bb906ad0a
User <231608@astanait.edu.kz>
2024-01-24 08:17:34 +0600

    assignment  1

--- src/models/Point.java
+++ src/models/Point.java
@@ -6,8 +6,22 @@ public class Point {
 
     // constructor having both parameters
     public Point(double x, double y){
+        setX(x);
+        setY(y);
+    }
+    public void setX(double x) {
         this.x = x;
+    }
+
+    public double getX() {
+        return x;
+    }
+    public void setY(double y) {
         this.y = y;
+    }
+
+    public double getY() {
+        return y;
     }
 
     // distance(Point dest) - distance from current point to the dest point
